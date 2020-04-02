1fde54efa571fa55ba2fda13d90d59f4e67cc34c
Mateus Fabricio <fabriciomateus05@gmail.com>
2020-04-02 15:08:13 -0300

    Adicionado o projeto Interface e Dicionario

---
+++ WindowsFormsApp1/InterfaceEDicionario/IPessoa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    public interface IPessoa
+    {
+        bool Falar(string mensagem);
+
+        bool Andar(int x, int y);
+    }
+
+
+}
---
+++ WindowsFormsApp1/InterfaceEDicionario/Kim.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    class Kim : IPessoa
+    {
+        public bool Andar(int x, int y)
+        {
+            MessageBox.Show("Andei");
+            return true;
+        }
+
+        public bool Falar(string mensagem)
+        {
+            MessageBox.Show(mensagem);
+            return true;
+        }
+    }
+
+    class Mateus : IPessoa
+    {
+        public bool Andar(int x, int y)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Falar(string mensagem)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
